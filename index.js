ad20ce627b0203d9d4b8cf5940f27ae7c466d2ae
Renanlanza <renan.giovani@live.com>
2025-02-23 20:18:12 -0300

    Prosseguindo com implementação de sessão, ainda em andamento.

--- index.js
+++ index.js
@@ -1,11 +1,35 @@
 import express from "express";
 import autenticar from "./security/autenticar.js";
+import session from "express-session";
+import { Cookie } from "express-session";
 const porta = 3000;
 const localhost = "0.0.0.0";
 const app = express();
 
 app.use(express.static("./public"));
 app.use(autenticar, express.static("./private"));
+app.use(session({
+    secret : "#My$3cr3tK3Y@!", //exemplo acadêmico, iremos estudar variáveis de ambiente para proteção futura.
+    resave : false,
+    saveUnitialized : false,
+    cookie: {
+        maxAge: 1000 * 60 * 15
+    }
+}));
+app.get("/login", (requisicao, resposta) => {
+    resposta.redirect("/login.html");
+});
+app.post("/login", (requisicao, resposta) => {
+    const usuario = requisicao.body.usuario;
+    const senha = requisicao.body.senha;
+    if (usuario === "admin" && senha === "admin") {
+        requisicao.session.autenticado = true;
+        resposta.redirect("/detalhes.html");
+    } else {
+        resposta.redirect("/login.html");
+    }
+
+});
 app.listen(porta, localhost, () => {
     console.log (`Servidor rodando em http://${localhost}:${porta}`);
 });
